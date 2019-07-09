15947e49981e0596206eef7f22e30cd23bc131c1
周凯 <201762041@qq.com>
2019-07-09 11:41:52 +0800

    ssss

---
+++ CCExtension/Classes/Model/test2.h
@@ -0,0 +1,17 @@
+//
+//  test2.h
+//  CCExtension
+//
+//  Created by MBP2015 on 2019/7/9.
+//  Copyright © 2019 MBP2015. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface test2 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CCExtension/Classes/Model/test2.m
@@ -0,0 +1,13 @@
+//
+//  test2.m
+//  CCExtension
+//
+//  Created by MBP2015 on 2019/7/9.
+//  Copyright © 2019 MBP2015. All rights reserved.
+//
+
+#import "test2.h"
+
+@implementation test2
+
+@end
