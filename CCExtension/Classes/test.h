cca414b276cadaf4369dbef3c22c31ec3d18d1d0
周凯 <201762041@qq.com>
2019-07-09 11:39:27 +0800

    123124

---
+++ CCExtension/Classes/test.h
@@ -0,0 +1,17 @@
+//
+//  test.h
+//  CCExtension
+//
+//  Created by MBP2015 on 2019/7/9.
+//  Copyright © 2019 MBP2015. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface test : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CCExtension/Classes/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  CCExtension
+//
+//  Created by MBP2015 on 2019/7/9.
+//  Copyright © 2019 MBP2015. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
